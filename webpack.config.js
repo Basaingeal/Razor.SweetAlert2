23a60b8ebfc819a313e6e252eac9f99bb07faf84
Michael J. Currie <mcurrie@agwsinc.com>
2019-07-22 09:04:03 -0500

    fix ts interface names

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-﻿const path = require('path');
+﻿const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
 module.exports = {
   entry: {
-    sweetAlert2: './src/ts/SweetAlert.ts',
-    'sweetAlert2.min': './src/ts/SweetAlert.ts',
-    darkTheme: './src/scss/dark-theme.scss',
-    'darkTheme.min': './src/scss/dark-theme.scss',
-    minimalTheme: './src/scss/minimal-theme.scss',
-    'minimalTheme.min': './src/scss/minimal-theme.scss',
-    borderlessTheme: './src/scss/borderless-theme.scss',
-    'borderlessTheme.min': './src/scss/borderless-theme.scss'
+    sweetAlert2: "./src/ts/SweetAlert.ts",
+    "sweetAlert2.min": "./src/ts/SweetAlert.ts",
+    darkTheme: "./src/scss/dark-theme.scss",
+    "darkTheme.min": "./src/scss/dark-theme.scss",
+    minimalTheme: "./src/scss/minimal-theme.scss",
+    "minimalTheme.min": "./src/scss/minimal-theme.scss",
+    borderlessTheme: "./src/scss/borderless-theme.scss",
+    "borderlessTheme.min": "./src/scss/borderless-theme.scss"
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, 'wwwroot')
+    path: path.resolve(__dirname, "wwwroot")
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.(sass|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         exclude: /node_modules/
       }
     ]
@@ -42,7 +38,7 @@ module.exports = {
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      path: path.resolve(__dirname, 'wwwroot')
+      path: path.resolve(__dirname, "wwwroot")
     }),
     new CleanWebpackPlugin()
   ],
@@ -53,6 +49,7 @@ module.exports = {
       }),
       new OptimizeCSSAssetsPlugin({
         assetNameRegExp: /\.min\.css$/
-      })]
+      })
+    ]
   }
 };
